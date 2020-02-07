e7ea0c18b1f659475b97608a432811a4d872e95a
MuyingLi <ml5333@nyu.edu>
2020-02-07 14:46:32 -0500

    Ryan write the code that enables cursor change, Selam changed the repo name and fix minor errors

--- cursify/content_scripts/cursify.js
+++ cursify/content_scripts/cursify.js
@@ -1,39 +1,49 @@
 (function() {
-
+  /**
    * Check and set a global guard variable.
    * If this content script is injected into the same page again,
    * it will do nothing next time.
+   */
+   
+  var images = [
+  'http://www.simpleimageresizer.com/_uploads/photos/3220be14/li_22x22.png',
+  'http://www.simpleimageresizer.com/_uploads/photos/3220be14/ryan_22x22.png',
+  'http://www.simpleimageresizer.com/_uploads/photos/3220be14/selam_22x22.png'];
+	
+  var x = 0;
    
   if (window.hasRun) {
     return;
   }
   window.hasRun = true;
+
   /**
-  * Given a URL to a beast image, remove all existing beasts, then
+   * Given a URL to a emoji image, remove all existing emojis, then
    * create and style an IMG node pointing to
    * that image, then insert the node into the document.
-  function insertBeast(cursorURL) {
-    removeCurrentCursor();
-    document.body.style('cursor:',cursorURL);
+   */
+  function insertEmoji(emojiURL) {
+	x = emojiURL;
+    document.body.style.cursor = 'url("' + images[x] + '"), default';
   }
 
+  /**
    * Remove every beast from the page.
-   
-  function removeCurrentCursor() {
-    document.body.style('cursor: default');
-
+   */
+  function removeExistingEmojis() {
+    document.body.style.cursor = "auto";
   }
 
-  
+  /**
    * Listen for messages from the background script.
-   * Call "beastify()" or "reset()".
-
+   * Call "emoji()" or "reset()".
+  */
   browser.runtime.onMessage.addListener((message) => {
-    if (message.command === "cursify") {
-      insertBeast(message.cursorURL);
+    if (message.command === "emoji") {
+      insertEmoji(message.emojiURL);
     } else if (message.command === "reset") {
-      removeCurrentCursor();
+      removeExistingEmojis();
     }
   });
-**/
+
 })();
---
+++ cursify/popup/emoji.js
@@ -0,0 +1,96 @@
+/**
+ * CSS to hide everything on the page,
+ * except for elements that have the "emoji-image" class.
+ */
+const hidePage = `body > :not(.emoji-image) {
+                    display: none;
+                  }`;
+
+/**
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
+function listenForClicks() {
+  document.addEventListener("click", (e) => {
+
+    /**
+     * Given the name of a beast, get the URL to the corresponding image.
+     */
+    function emojiNameToURL(emojiName) {
+      switch (emojiName) {
+        case "🔨":
+          return 0;
+        case "😐":
+          return 1;
+        case "🤯":
+          return 2;
+      }
+    }
+
+    /**
+     * Insert the page-hiding CSS into the active tab,
+     * then get the beast URL and
+     * send a "emoji" message to the content script in the active tab.
+     */
+    function emoji(tabs) {
+        let url = emojiNameToURL(e.target.textContent);
+        browser.tabs.sendMessage(tabs[0].id, {
+          command: "emoji",
+          emojiURL: url
+        });
+    }
+
+    /**
+     * Remove the page-hiding CSS from the active tab,
+     * send a "reset" message to the content script in the active tab.
+     */
+    function reset(tabs) {
+      browser.tabs.removeCSS({code: hidePage}).then(() => {
+        browser.tabs.sendMessage(tabs[0].id, {
+          command: "reset",
+        });
+      });
+    }
+
+    /**
+     * Just log the error to the console.
+     */
+    function reportError(error) {
+      console.error(`Could not emotify: ${error}`);
+    }
+
+    /**
+     * Get the active tab,
+     * then call "emoji()" or "reset()" as appropriate.
+     */
+    if (e.target.classList.contains("emoji")) {
+      browser.tabs.query({active: true, currentWindow: true})
+        .then(emoji)
+        .catch(reportError);
+    }
+    else if (e.target.classList.contains("reset")) {
+      browser.tabs.query({active: true, currentWindow: true})
+        .then(reset)
+        .catch(reportError);
+    }
+  });
+}
+
+/**
+ * There was an error executing the script.
+ * Display the popup's error message, and hide the normal UI.
+ */
+function reportExecuteScriptError(error) {
+  document.querySelector("#popup-content").classList.add("hidden");
+  document.querySelector("#error-content").classList.remove("hidden");
+  console.error(`Failed to execute emoji content script: ${error.message}`);
+}
+
+/**
+ * When the popup loads, inject a content script into the active tab,
+ * and add a click handler.
+ * If we couldn't inject the script, handle the error.
+ */
+browser.tabs.executeScript({file: "/content_scripts/cursify.js"})
+.then(listenForClicks)
+.catch(reportExecuteScriptError);
